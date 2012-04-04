adafce91989d7a2219188b01603e9aaaf0299e86
Terry Poulin <t.poulin@wegener.com>
2012-04-04 19:04:16 +0000

    Rewrite of tpsh_cgen; if/elif/else now works!
    Also all other code structures temporarily dropped.

--- tests.d/if.sh
+++ tests.d/if.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env tpsh
 
-# on one line because tpsh can't handle the other way yet.
-if echo 'running first if test'; then echo "if passed"; elif echo "elif"; then echo "elif passed"; else echo "else passed"; fi
+# these are all on one line because tpsh can't handle the other way yet.
 
-echo 'running second if test'
-# the embedded ' break this
-#if supercalifragilisticexpialidocious; then echo "if doesn't work"; elif echo "elif"; then echo "elif passed"; else echo "else passed"; fi
-if supercalifragilisticexpialidocious; then echo "if command - passed"; else "if command - failed"; fi
+if echo "testing basic if statement for truth"; then echo "pass"; fi
+# unlike 'if false; then ...' this also kind of will explode if we frakup the
+# entire syntax-handling code enough for the embedded keywords to get picked up
+# wrong.
+if testing basic if statement for falsehood; then echo "fail"; fi
+
+
+echo "testing if-else statement"
+if false; then echo fail; else echo pass; fi
+
+echo "testing if-elsif-else statement for elsif-truth"
+if false; then echo "failed in if-then"; elif true; then echo pass; else echo "failed in else"; fi
+
+echo "Running regression test for tpsh_cgen()."
+# the embedded ' made the xxxxx version of tpsh_cgen generate bad code.
+if supercalifragilisticexpialidocious; then echo "if doesn't work"; elif echo "elif"; then echo "elif passed"; else echo "else passed"; fi
+
