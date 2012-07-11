f12f7d78623e6e4b6a4b3e3b1c55d92c2836c46d
Terry M. Poulin <BigBoss1964@gmail.com>
2012-07-11 21:13:51 +0000

    Delay backtick expansion; fixes loops!

--- tests.d/loops.sh
+++ tests.d/loops.sh
@@ -4,21 +4,14 @@
 while true; do echo "Very simple while+break"; break; done
 until false; do echo "Very simple until+break"; break; done
 
-# tpsh can't handle this yet.
+# tpsh can't handle this without an external test app, because test is not
+# fully implemented yet. It's just a dummy that returns false.
 #
-#   - test is not implemented.
-#   - variables are interpolated to soon.
-#
-# while test "$i" -gt 0 ; generates perl code like while ( resolve_cmd("/usr/bin/test", "10", "-gt", "0") == 0 )
-#
-# So this can't work properly because the test will always become `10 > 0`. 
-#
-
 setenv i 10
-while test "$i" -gt 0 ; do echo "while-test: $i"; setenv i `expr $i - 1`; done
-until test "$i" -gt 10 ; do echo "until-test: $i"; i=`expr $i + 1`; done
+while /usr/bin/test "$i" -gt 0 ; do echo "while-test: $i"; setenv i `expr $i - 1`; done
+until /usr/bin/test "$i" -gt 10 ; do echo "until-test: $i"; setenv i `expr $i + 1`; done
 
 
-# not even supported yet by the new code generator implementation
+# not supported yet by the code generator implementation
 for lv in a b c d e f g; do echo $lv; done
 
